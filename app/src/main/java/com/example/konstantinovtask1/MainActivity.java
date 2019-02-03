129a4551496ae66a9f26edd40c02f85daa091b61
Nickolay Konstantinov <konst007@tut.by>
2019-02-03 16:22:37 +0300

    dev: version

--- app/src/main/java/com/example/konstantinovtask1/MainActivity.java
+++ app/src/main/java/com/example/konstantinovtask1/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.konstantinovtask1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
 import com.crashlytics.android.Crashlytics;
 import io.fabric.sdk.android.Fabric;
 import com.microsoft.appcenter.AppCenter;
@@ -16,5 +18,7 @@ public class MainActivity extends AppCompatActivity {
         Fabric.with(this, new Crashlytics());
         AppCenter.start(getApplication(), "cd0d67f1-e677-4354-af98-1f6eade8383d", Analytics.class, Crashes.class);
         setContentView(R.layout.activity_main);
+        final TextView version = findViewById(R.id.version);
+        version.setText(BuildConfig.VERSION_NAME);
     }
 }
